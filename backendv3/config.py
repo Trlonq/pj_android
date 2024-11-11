28cf0bd40ef6104054681d18a9831b2563ad1a52
tlon1210 <163876920+Trlonq@users.noreply.github.com>
2024-11-11 18:05:34 +0700

    url
    url

--- backendv3/config.py
+++ backendv3/config.py
@@ -1,8 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL",
-                                        "postgresql://avnadmin:AVNS_K5PEAARi4daVtaQ1PHB@potholedb-trlonq.d.aivencloud.com:24953/defaultdb?sslmode=require")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwtsecretkey")
